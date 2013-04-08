c932f92e8399dcbc80857a608278099d2d9b933e
gaopengxiang417 <gpx417@hotmail.com>
2013-04-08 14:10:55 +0800

    添加了备注

--- src/com/gao/first/FunctionInterfaceTest.java
+++ src/com/gao/first/FunctionInterfaceTest.java
@@ -4,7 +4,8 @@ package com.gao.first;
  * User: wangchen.gpx
  * Date: 13-4-8
  * Time: 下午1:44
- * 函数接口一般不可以覆盖object类中的方法
+ * 函数接口一般不可以覆盖object类中的方法,主要的考虑是:
+ * 一般是Object对象中的方法一般会有子类进行覆盖，不需要进行接口中进行重写
  */
 @FunctionalInterface
 public interface FunctionInterfaceTest {
