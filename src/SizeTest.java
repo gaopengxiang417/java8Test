c4f0948906d61474a760bc97e587710d5ae63926
gaopengxiang417 <gaopengxiang417@163.com>
2013-12-21 10:31:01 +0800

    java8的calendar.build的学习

---
+++ src/SizeTest.java
@@ -0,0 +1,19 @@
+/**
+ * User: wangchen.gpx
+ * Date: 13-7-8
+ * Time: 下午1:32
+ */
+public class SizeTest {
+    public static void main(String[] args) {
+        int[] a = {3,5,6};
+        System.out.println(a[8]);
+        /*String s = "2345667";
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 100; i++) {
+            stringBuilder.append(s);
+        }
+
+        String s1 = stringBuilder.toString();
+        System.out.println(s1.getBytes().length);*/
+    }
+}
