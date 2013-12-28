741c4cdc67894ad9a32689b170d5075c564b6553
gaopengxiang417 <gaopengxiang417@163.com>
2013-12-28 19:39:55 +0800

    添加了对于posix的快捷方式

---
+++ src/com/gao/first/PosfixTeset.java
@@ -0,0 +1,84 @@
+package com.gao.first;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * User: wangchen.gpx
+ * Date: 13-12-28
+ * Time: 下午7:01
+ */
+public class PosfixTeset {
+    public static void main(String[] args) {
+        //for循环的快捷方式
+        ArrayList<String> list = new ArrayList<>();
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+
+
+        //
+
+
+        //xxx.instanceof
+//        list instanceof LinkedList ? ((LinkedList) list) : null;
+
+        //xxx.return
+//        return list;
+
+        //加小括号 xxx.par
+//        (list);
+
+        //反响循环 xxx.forr
+        for (int i = list.size() - 1; i >= 0; i--) {
+
+        }
+        //for xxx.fori
+        for (int i = 0; i < list.size(); i++) {
+
+        }
+
+        //new xxx.new
+        ArrayList arrayList = new ArrayList();
+
+        //强制转换类型 xx.cast
+        for (Object s : ((ArrayList) list)) {
+
+        };
+
+        //while快捷键 xx.while
+        while (list.isEmpty())
+
+        //list for each xx.for
+        for (String s : list) {
+            System.out.println(s);
+        }
+
+        //布尔表达式的方向 xx.not
+//        1 != 1;
+
+        //if not null xx.notnull
+        if (list != null) {
+
+        }
+
+        //if null xx.null
+        if (list == null) {
+
+        }
+
+        //变量的声明和初始化 xxx.var
+        String sdsf = "sdsf";
+
+        //if not true xx.else
+        if (1 != 1) {
+
+        }
+
+        //if的语句 xx.if
+        if (1 == 1) {
+
+        }
+    }
+}
