3d3942ee2a8147a578b3e1a0ba0405786457f40c
gaopengxiang417 <gaopengxiang417@163.com>
2014-01-02 19:56:08 +0800

    添加了对于java8的默认方法的支持

---
+++ src/com/gao/first/C.java
@@ -0,0 +1,48 @@
+package com.gao.first;
+
+/**
+ * User: wangchen.gpx
+ * Date: 14-1-2
+ * Time: 下午7:44
+ */
+public class C implements A,B{
+    public static void main(String[] args) {
+        C c = new C();
+        c.hello();
+
+        F f = new F();
+        f.hello();
+    }
+}
+
+//这里F必须重写默认的方法，否则会报错的
+class F implements D,E{
+    @Override
+    public void hello() {
+        D.super.hello();
+    }
+}
+
+interface E{
+    default void hello(){
+        System.out.println("E method....");
+    }
+}
+interface D{
+    default void hello(){
+        System.out.println("D method...");
+    }
+}
+
+//这里可以通过B实现A来避免多重继承的冲突，调用的时候会使用就近原则
+interface A{
+    default void hello(){
+        System.out.println("a method...");
+    }
+}
+
+interface B extends A{
+    default void hello(){
+        System.out.println("b method....");
+    }
+}
