f8b15f814fefbfd82131024a9495c18d3fbfe56c
gaopengxiang417 <gaopengxiang417@163.com>
2016-03-09 23:38:12 +0800

    添加了对于自定义Collector的功能

--- src/com/gao/java8inaction/stream/CollectorsTest.java
+++ src/com/gao/java8inaction/stream/CollectorsTest.java
@@ -1,6 +1,11 @@
 package com.gao.java8inaction.stream;
 
 import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -187,7 +192,52 @@ public class CollectorsTest {
         System.out.println(collect27);
 
 
+        //调用自定义的collector
+        Map<Boolean, List<Integer>> collect28 = IntStream.rangeClosed(2, 60).boxed().collect(new PrimeNumbersCollector());
+        System.out.println(collect28);
 
+    }
+
+    static class PrimeNumbersCollector implements Collector<Integer, Map<Boolean, List<Integer>>, Map<Boolean, List<Integer>>> {
+        @Override
+        public Supplier<Map<Boolean, List<Integer>>> supplier() {
+            return () -> new HashMap(){
+                {
+                    put(true, new ArrayList<>());
+                    put(false, new ArrayList<>());
+                }
+            };
+        }
+
+        @Override
+        public BiConsumer<Map<Boolean, List<Integer>>, Integer> accumulator() {
+            return (Map<Boolean, List<Integer>> accumulator, Integer candidate) -> {
+                accumulator.get(true).add(candidate);
+            };
+        }
+
+        @Override
+        public BinaryOperator<Map<Boolean, List<Integer>>> combiner() {
+
+            return (Map<Boolean, List<Integer>> map1, Map<Boolean, List<Integer>> map2) -> {
+
+                map1.get(true).addAll(map2.get(true));
+                map1.get(false).addAll(map2.get(false));
+
+                return map1;
+            };
+        }
+
+        @Override
+        public Function<Map<Boolean, List<Integer>>, Map<Boolean, List<Integer>>> finisher() {
+
+            return Function.identity();
+        }
+
+        @Override
+        public Set<Characteristics> characteristics() {
+            return Collections.unmodifiableSet(EnumSet.of(Characteristics.IDENTITY_FINISH));
+        }
     }
 
     public enum CaloricLevel{DIET, NORMAL, FAT}
